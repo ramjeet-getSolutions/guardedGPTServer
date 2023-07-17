c3216a21c1e5f6e8227df4a7ad9e047fed2638f6
ramjeet-getSolutions <ramjeetvish@gmail.com>
2023-07-17 15:21:35 +0530

    update.

--- constants.py
+++ constants.py
@@ -4,9 +4,12 @@ from chromadb.config import Settings
 
 load_dotenv()
 
+# Define the folder for storing database
+PERSIST_DIRECTORY = os.environ.get('PERSIST_DIRECTORY')
+
 # Define the Chroma settings
 CHROMA_SETTINGS = Settings(
         chroma_db_impl='duckdb+parquet',
-        persist_directory="db/",
+        persist_directory=PERSIST_DIRECTORY,
         anonymized_telemetry=False
 )
